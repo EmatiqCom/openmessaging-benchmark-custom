ec56e8c0e2c66b525c7fc05aeeda66a6997381d4
David Janicek <davidak09@gmail.com>
2024-11-06 16:57:33 +0100

    feat: redpanda results

---
+++ ematiq/benchmarks/redpanda/1_broker/ematiq-workload-redpanda-acks-0.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+NAME="100_topics_1_partition_1Kb_1_broker_acks_0"
+
+echo "Writing workload files $NAME"
+
+cat > ematiq_workload_$NAME.yaml << EOF
+name: $NAME
+
+topics: 100
+partitionsPerTopic: 1
+messageSize: 1024
+payloadFile: "payload/payload-1Kb.data"
+subscriptionsPerTopic: 2
+consumerPerSubscription: 1
+producersPerTopic: 2
+producerRate: 500
+consumerBacklogSizeGB: 0
+testDurationMinutes: 25
+warmupDurationMinutes: 5
+EOF
+
+cat > ematiq_config_$NAME.yaml << EOF
+name: Redpanda
+driverClass: io.openmessaging.benchmark.driver.redpanda.RedpandaBenchmarkDriver
+# Kafka client-specific configuration
+replicationFactor: 1
+reset: true
+
+topicConfig: |
+commonConfig: |
+  bootstrap.servers=10.0.0.130:9092
+  request.timeout.ms=120000
+producerConfig: |
+  acks=0
+consumerConfig: |
+  auto.offset.reset=earliest
+  enable.auto.commit=false
+EOF
+
+echo "Starting benchmark $NAME"
+
+sudo bin/benchmark -d ematiq_config_$NAME.yaml --output ematiq_workload_redpanda_$NAME.json ematiq_workload_$NAME.yaml
+
+echo "Benchmark complete $NAME"
